8bbcb3cdacc98e75d799eeade584411be1051941
Suneel Chakravorty <suneel0101@gmail.com>
2013-02-08 19:02:54 -0500

    adding unit test to check that restroom.urls.urlpatterns = api.url_patterns

---
+++ tests/unit/test_urls.py
@@ -0,0 +1,11 @@
+from sure import expect
+
+from restroom.urls import urlpatterns
+from restroom import api
+
+from tests.unit.test_api import transform_to_attrs_dict
+
+
+def test_restroom_urls_equals_api_url_patterns():
+    (expect(transform_to_attrs_dict(urlpatterns))
+     .to.equal(transform_to_attrs_dict(api.url_patterns)))
