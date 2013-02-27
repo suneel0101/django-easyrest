352325854a3b006bf640fa871225b9078ecf89c5
Suneel Chakravorty <suneel0101@gmail.com>
2013-02-27 02:18:51 -0500

    refactoring view slightly

--- restroom/views.py
+++ restroom/views.py
@@ -3,19 +3,21 @@ import json
 from django.http import HttpResponse, HttpResponseForbidden
 from django.views.generic import View
 
-response = lambda data: HttpResponse(json.dumps(data),
-                                     mimetype="application/json")
-forbidden = HttpResponseForbidden
+
+class BaseRestroomView(View):
+    def get_response(self, data):
+        if self.http_method in self.resource.http_methods:
+            return HttpResponse(json.dumps(data), mimetype="application/json")
+        else:
+            return HttpResponseForbidden()
+
+    def dispatch(self, request, *args, **kwargs):
+        self.http_method = request.method
+        return super(BaseRestroomView, self).dispatch(request, *args, **kwargs)
 
 
-class RestroomMixin(object):
-    def allowed(self, method):
-        return method in self.resource.http_methods
-
-
-class RestroomListView(View, RestroomMixin):
+class RestroomListView(BaseRestroomView):
     resource = None
 
     def get(self, request, *args, **kwargs):
-            return (response(self.resource.retrieve())
-                    if self.allowed("GET") else forbidden())
+        return self.get_response(self.resource.retrieve())
