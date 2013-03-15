c02ca3249460ef800e162065cc8f475b4723cbcd
Suneel Chakravorty <suneel0101@gmail.com>
2013-03-14 20:25:20 -0400

    fixing serialization bug of Decimal and Date fields

--- restroom/utils.py
+++ restroom/utils.py
@@ -1,10 +1,19 @@
+import json
 import datetime
+from decimal import Decimal
 
 
 def get_val(obj, name):
     value = getattr(obj, name)
-    if isinstance(value, datetime.datetime):
+    if (isinstance(value,
+                   datetime.datetime) or isinstance(value, datetime.date)):
         return value.isoformat()
     elif isinstance(value, bool):
         return int(value)
+    elif isinstance(value, Decimal):
+        return float(value)
+    try:
+        json.dumps(value)
+    except TypeError:
+        return "Could not serialize {}".format(str(value))
     return value
