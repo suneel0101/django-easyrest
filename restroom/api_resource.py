56ecd75e7194f76cbcb09de1e6ad36b183f9633c
suneel0101 <suneel0101@gmail.com>
2013-05-10 01:20:23 -0400

    reordering methods in resource class

--- restroom/api_resource.py
+++ restroom/api_resource.py
@@ -16,11 +16,6 @@ class APIResource(object):
     def get_queryset(self):
         return self.model.objects.all()
 
-    def filter_for_user(self, qs, user):
-        if user and self.filter_by_user_path:
-            qs = qs.filter(**{self.filter_by_user_field: user.id})
-        return qs
-
     def get_list(self, user=None):
         qs = self.filter_for_user(self.get_queryset(), user)
         return {
@@ -39,6 +34,11 @@ class APIResource(object):
             user.id != self.get_user_id(item)):
             return {"error": "You do not have access to this data"}
         return self.serialize(item)
+
+    def filter_for_user(self, qs, user):
+        if user and self.filter_by_user_path:
+            qs = qs.filter(**{self.filter_by_user_field: user.id})
+        return qs
 
     def get_user_id(self, item):
         sequence = self.filter_by_user_path.split("__")
