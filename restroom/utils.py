a44bd194e3436cc140689536338b453be3c798b0
suneel0101 <suneel0101@gmail.com>
2013-05-10 01:23:19 -0400

    removing get_val from utils

--- restroom/utils.py
+++ restroom/utils.py
@@ -1,7 +1,3 @@
-import json
-import datetime
-from decimal import Decimal
-from django.db import models
 from .models import APIKey
 
 
@@ -18,21 +14,3 @@ def authorize(request):
         else:
             user = api_key.user
     return user
-
-
-def get_val(obj, name):
-    value = getattr(obj, name)
-    if (isinstance(value,
-                   datetime.datetime) or isinstance(value, datetime.date)):
-        value = value.isoformat()
-    elif isinstance(value, bool):
-        value = int(value)
-    elif isinstance(value, Decimal):
-        return float(value)
-    elif isinstance(value, models.Model):
-        value = value.pk
-    try:
-        json.dumps(value)
-    except TypeError:
-        return "Could not serialize {}".format(str(value))
-    return value
